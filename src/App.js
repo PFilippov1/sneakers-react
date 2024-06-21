1d8e1312819ffdff2b3582fe4ac8839d4d836594
PFilippov1 <p.filippov2.20@gmail.com>
2024-06-21 22:23:25 +0300

    sneakers layout + modules

--- src/App.js
+++ src/App.js
@@ -2,6 +2,69 @@ import Card from "./components/Card";
 import Header from "./components/Header";
 import Drawer from "./components/Drawer";
 
+const arr = [
+  {
+    title: "Men`s sneakers Nike Blazer Mid Suede",
+    price: 120,
+    imageUrl: "/assets/sneakers/1.jpg",
+  },
+  {
+    title: "Men`s sneakers Air Max 270",
+    price: 130,
+    imageUrl: "/assets/sneakers/2.jpg",
+  },
+  {
+    title: "Men`s sneakers Nike Blazer Mid Suede",
+    price: 140,
+    imageUrl: "/assets/sneakers/3.jpg",
+  },
+  {
+    title: "Men`s sneakers Puma X Aka Boku Future Rider",
+    price: 110,
+    imageUrl: "/assets/sneakers/4.jpg",
+  },
+  {
+    title: "Men`s sneakers Under Armour Curry 8",
+    price: 130,
+    imageUrl: "/assets/sneakers/5.jpg",
+  },
+  {
+    title: "Men`s sneakers Nike Kyrie 7",
+    price: 150,
+    imageUrl: "/assets/sneakers/6.jpg",
+  },
+  {
+    title: "Men`s sneakers Jordan Air Jordan 11",
+    price: 100,
+    imageUrl: "/assets/sneakers/7.jpg",
+  },
+  {
+    title: "Men`s sneakers Nike LeBron XVIII",
+    price: 120,
+    imageUrl: "/assets/sneakers/8.jpg",
+  },
+  {
+    title: "Men`s sneakers Nike ",
+    price: 130,
+    imageUrl: "/assets/sneakers/9.jpg",
+  },
+  {
+    title: "Men`s sneakers Nike Kyrie Flytrap",
+    price: 110,
+    imageUrl: "/assets/sneakers/10.png",
+  },
+  {
+    title: "Men`s sneakers Nike Flytrap IV",
+    price: 130,
+    imageUrl: "/assets/sneakers/11.png",
+  },
+  {
+    title: "Men`s sneakers Nike ",
+    price: 100,
+    imageUrl: "/assets/sneakers/12.jpg",
+  },
+];
+
 function App() {
   return (
     <div className="wrapper clear">
@@ -17,7 +80,19 @@ function App() {
         </div>
 
         <section className="sneakers d-flex">
-          <Card />
+          {/* <Card
+            title="Men`s sneakers Nike Blazer Mid Suede"
+            price={120}
+            imageUrl="/assets/sneakers/1.jpg"
+          />
+           */}
+          {arr.map((card) => (
+            <Card
+              title={card.title}
+              price={card.price}
+              imageUrl={card.imageUrl}
+            />
+          ))}
         </section>
       </div>
     </div>
--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,20 +1,22 @@
-function Card() {
+import styles from './card.module.scss'
+
+function Card(props) {
   return (
-    <div className="card">
-      <div className="favorite">
+    <div className={styles.card}>
+      <div className={styles.favorite}>
         <img src="/assets/img/heart-unliked.svg" alt="Unliked"></img>
       </div>
       <img
         width={133}
         height={112}
-        src="/assets/sneakers/1.jpg"
+        src={props.imageUrl}
         alt="sneakers"
       />
-      <h5>Men`s nike sneakers</h5>
+      <h5>{props.title}</h5>
       <div className="d-flex justify-between align-center">
         <div className="d-flex flex-column ">
           <span>Price:</span>
-          <b>120 $</b>
+          <b>{props.price}$</b>
         </div>
 
         <button className="button">
